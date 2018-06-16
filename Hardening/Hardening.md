32247cc1b20699aba9f0a67c814e366380ccadbd
Clecio <clecioanderson@gmail.com>
2018-06-15 23:46:32 -0300

    atualizando

--- Hardening/Hardening.md
+++ Hardening/Hardening.md
@@ -1,3 +1,4 @@
 # Hardening
 
 1 - Localizar programas desnecessários instalados e listá-los [dpkg]
+2 - Identificar arquivos com suid, sgid e sticky bit habilitados
--- Hardening/hardening.sh
+++ Hardening/hardening.sh
@@ -8,7 +8,12 @@
 
 ####  Localizar Servicos Desnecessarios ####
 
-dpkg -l | awk '{print $2,$3}' | sed "1,5d" > /home/$USER/servicosinstalados.lst
+dpkg -l | awk '{print $2,$3}' | sed "1,5d" > /tmp/servicosinstalados.lst
+
+### Localizar arquivos com SUID, SGID e STICKY bit ativados ###
+
+echo "Localizando arquivos..."
+find / -perm 4000 > /tmp/listarquivos_sbit.lst
 
 
 
